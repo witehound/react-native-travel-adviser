387292f9886bc7a1f92653cc1fccdc14df876e4d
witehound <netvid00@gmail.com>
2023-02-04 16:27:47 +0100

    added asset folder

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import { Text, SafeAreaView } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen } from "./screens";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView>
-        <Text>Hello World</Text>
-      </SafeAreaView>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
