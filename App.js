09292fb8923d279a311c0bf53eb7024e4107b484
witehound <netvid00@gmail.com>
2023-02-04 15:30:58 +0100

    added tailwind config

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { TailwindProvider } from "tailwindcss-react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is jsut the intro</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TailwindProvider>
+      <View style="">
+        <Text>This is jsut the intro</Text>
+        <StatusBar style="auto" />
+      </View>
+    </TailwindProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
